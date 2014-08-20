21ace913d1fb6aa00cc5701670f66ea05d60a87b
arcan1s <esalexeev@gmail.com>
2014-08-20 17:23:03 +0400

    remove sudo Why I use it? I don't know, I'm idiot =)

--- extract_pkglist.py
+++ extract_pkglist.py
@@ -23,42 +23,42 @@ if __name__ == "__main__":
                         help = 'Do not show output',
                         action = 'store_true', default = False)
     args = parser.parse_args()
-    
+
     pkglist_repo = open(args.prefix + "_" + args.suf_repo, 'w')
     pkglist_other = open(args.prefix + "_" + args.suf_other, 'w')
     if (not args.test):
         pkglist_nondb = open(args.prefix + "_" + args.suf_nondb, 'w')
-    
+
     repos = 0
     other = 0
-    
-    for line in os.popen("sudo pacman -Qenq"):
+
+    for line in os.popen("pacman -Qenq"):
         if (not args.quiet):
             print "Copying: '" + line[:-1] + "'"
         repos += 1
         pkglist_repo.write(line)
-    for line in os.popen("sudo pacman -Qemq"):
+    for line in os.popen("pacman -Qemq"):
         if (not args.quiet):
             print "Copying: '" + line[:-1] + "'"
         other += 1
         if (args.test):
             pkglist_other.write(line)
         else:
-            num_lines = sum(1 for testline in 
-                            os.popen("wget --spider -o /dev/stdout https://aur.archlinux.org/packages/" + 
+            num_lines = sum(1 for testline in
+                            os.popen("wget --spider -o /dev/stdout https://aur.archlinux.org/packages/" +
                             line[:-1] + " | grep '404 Not Found'"))
             if (num_lines == 0):
                 pkglist_other.write(line)
             else:
                 pkglist_nondb.write(line)
-    
+
     if (not args.quiet):
         print "---------------------"
         print "--------Done!--------"
         print "Repos:          %5s" % (str(repos))
         print "Other:          %5s" % (str(other))
         print "Total packages: %5s" % (str(repos + other))
-    
+
     pkglist_repo.close()
     pkglist_other.close()
     if (not args.test):
